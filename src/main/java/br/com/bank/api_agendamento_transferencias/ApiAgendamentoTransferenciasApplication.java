3c3252d4b306458d84f69449632021418a3efcf7
Anderson <aferrigocursos@gmail.com>
2025-09-03 20:58:08 -0300

    initial commit

---
+++ src/main/java/br/com/bank/api_agendamento_transferencias/ApiAgendamentoTransferenciasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank.api_agendamento_transferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendamentoTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendamentoTransferenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bank/api_agendamento_transferencias/ApiAgendamentoTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bank.api_agendamento_transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendamentoTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
